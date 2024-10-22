c55ce585f27bfb491e83c8230815f8f01166d431
masashi381 <121969638+masashi381@users.noreply.github.com>
2024-10-22 08:48:38 -0700

    set variable

--- calculators/Calculator.java
+++ calculators/Calculator.java
@@ -1,7 +1,16 @@
 package calculators;
+import java.io.*;
 
 public class Calculator {
-  public static void main(String[] args) {
-    System.out.println("test"); //
+  public static void main(String[] args) throws IOException {
+   // variable
+   int i = 1;
+  
+    while(i <= 5){
+      System.out.println("Add?Sub?Multiple?Divide? Please type A for Add, S for Sub, M for Multiple, or D for Divide");
+      InputStreamReader isr = new InputStreamReader(System.in);
+      BufferedReader br = new BufferedReader(isr);
+      String calcType = br.readLine();
+    }
   }
-};
+}; 
