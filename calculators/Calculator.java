8c7d994c915c8b0d2653959a17a802fa28fbd2d2
masashi381 <121969638+masashi381@users.noreply.github.com>
2024-10-24 09:52:11 -0700

    calculator is done

--- calculators/Calculator.java
+++ calculators/Calculator.java
@@ -12,7 +12,7 @@ public class Calculator {
       BufferedReader br = new BufferedReader(isr);
       String calcType = br.readLine();
 
-      if(calcType.equals("A")){
+      if(calcType.equals("A") || calcType.equals("a")){
         System.out.println("Please type first Number for Add");
 
         BufferedReader addBr = new BufferedReader(isr);
@@ -25,8 +25,8 @@ public class Calculator {
         String str2 = addBr2.readLine();
         float num2 = Integer.parseInt(str2);
         System.out.println( "Second number is " + num2 );
-        System.out.println("Result is " + (num + num2));
-      } else if(calcType.equals("S")){
+        System.out.println(num + " + " + num2 + " = " + (num + num2));
+      } else if(calcType.equals("S") || calcType.equals("s")){
         System.out.println("Please type first Number for Sub");
 
         BufferedReader subBr = new BufferedReader(isr);
@@ -39,8 +39,8 @@ public class Calculator {
         String str2 = subBr2.readLine();
         float num2 = Integer.parseInt(str2);
         System.out.println( "Second number is " + num2 );
-        System.out.println("Result is " + (num - num2));
-      } else if(calcType.equals("M")) {
+        System.out.println(num + " - " + num2 + " = " + (num - num2));
+      } else if(calcType.equals("M") || calcType.equals("m")) {
         System.out.println("Please type first Number for Multiple");
 
         BufferedReader mulBr = new BufferedReader(isr);
@@ -53,34 +53,48 @@ public class Calculator {
         String str2 = mulBr2.readLine();
         float num2 = Integer.parseInt(str2);
         System.out.println( "Second number is " + num2 );
-        System.out.println("Result is " + (num * num2));
-      } else if(calcType.equals("D")){
-        try{
-          System.out.println("Please type first Number for Divide");
-  
-          BufferedReader divBr = new BufferedReader(isr);
-          String str = divBr.readLine();
-          float num = Integer.parseInt(str);
-          System.out.println( "first number is " + num );
-  
-          System.out.println("Please type second Number for Divide");
-          BufferedReader divBr2 = new BufferedReader(isr);
-          String str2 = divBr2.readLine();
-          float num2 = Integer.parseInt(str2);
-          System.out.println( "Second number is " + num2 );
-          System.out.println("Result is " + (num / num2));
-        } catch(ArithmeticException e) {
-          System.out.println("Error: Division by zero");
-        }
+        System.out.println(num + " * " + num2 + " = "  + (num * num2));
+      } else if(calcType.equals("D") || calcType.equals("d")){
+        System.out.println("Please type first Number for Divide");
 
-        // if(num2 == 0){
-        //   System.out.println("Error: Division by zero");
-        // } else {
-        //   System.out.println("Result is " + (num / num2));
-        // }
+        BufferedReader divBr = new BufferedReader(isr);
+        String str = divBr.readLine();
+        float num = Integer.parseInt(str);
+        System.out.println( "first number is " + num );
+
+        System.out.println("Please type second Number for Divide");
+        BufferedReader divBr2 = new BufferedReader(isr);
+        String str2 = divBr2.readLine();
+        float num2 = Integer.parseInt(str2);
+        System.out.println( "Second number is " + num2 );
+        // System.out.println(num + " / " + num2 + " = " + (num / num2));
+
+        if(num2 == 0){
+          System.out.println("Error: Division by zero");
+        } else {
+          System.out.println(num + " / " + num2 + " = " + (num / num2));
+        }
       } else {
         System.out.println("Invalid input. Please try again.");
       }
+      String again = "";
+      while(i <= 2){
+        System.out.println("Do you want to continue? (Y/N)");
+        BufferedReader againBr = new BufferedReader(isr);
+        again = againBr.readLine();
+        if(again.equals("Y") || again.equals("y")){
+          break;
+        } else if(again.equals("N") || again.equals("n")){
+          // System.out.println("Exiting...");
+          break;
+        } else {
+          System.out.println("Invalid input. Please try again.");
+        }
+      }
+      if(again.equals("N")){
+        break;
+      }
     }
+    System.out.println("done");
   }
 }; 
